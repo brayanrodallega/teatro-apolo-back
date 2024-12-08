35cf924c3cd2b58ccb1129bdaf7ff6c93c2005a5
Brayan Rodallega <thebrayan1a1@outlook.com>
2024-12-07 21:37:56 -0500

    fix cors permits

--- app.js
+++ app.js
@@ -12,13 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(require("cookie-parser")());
 
-
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true,
-};
-  
-app.use(cors(corsOptions));
+// Use CORS middleware
+app.use(cors());
 
 
 // Rutas
